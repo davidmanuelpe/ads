1bb41dd9fdc32a61dc5d986aa6c04f138d2c45ea
matheus <matheus.noronha@solyd.com.br>
2021-12-01 12:48:43 -0300

    feat: limit computer connections

--- src/DragNDrop/index.tsx
+++ src/DragNDrop/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, DragEvent, MouseEvent } from 'react';
+import React, { useState, DragEvent, MouseEvent, FC } from 'react';
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
@@ -6,13 +6,23 @@ import ReactFlow, {
   Controls,
   OnLoadParams,
   Elements,
-  Connection,
   Edge,
   ElementId,
   Node,
   FlowElement,
+  Handle,
+  Position,
+  NodeProps,
+  NodeTypesType,
+  Connection,
 } from 'react-flow-renderer';
-import { nodeTypes } from './CustomNodes'
+
+import computador from '../imagens/computador.jpg';
+import cpu from '../imagens/CPU.png';
+import disk from '../imagens/Disk.jpg';
+import memory from '../imagens/memory.png';
+import network from '../imagens/network.png';
+import process from '../imagens/Processes.png';
 
 
 import Sidebar from './Sidebar';
@@ -21,19 +31,153 @@ import './dnd.css';
 
 
 let id = 0;
-export const getId = (): ElementId => `dndnode_${id++}`;
+export const getId = (): ElementId => `node_${id++}`;
 
 const DnDFlow = () => {
   const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
   const [elements, setElements] = useState<Elements>([]);
-  const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
+  const onConnect = (params: Connection | Edge) => {
+    const { source, target } = params
+    if (!source || !target) {
+      return null
+    }
+    addEdgeInConnections(source, target)
+    setElements((els) => addEdge(params, els));
+  }
   const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
   const onLoad = (_reactFlowInstance: OnLoadParams) => setReactFlowInstance(_reactFlowInstance);
   const onElementClick = (_: MouseEvent, element: FlowElement) => {
     console.log(element)
   }
 
+  const addEdgeInConnections = function (source: string, target: string): void | null {
+    const computer = getElement(target)
+    const component = getElement(source)
+    if (!computer || !component) {
+      return null
+    }
+    computer?.data.connections.push({ type: component?.type, id: component.id })
+    component?.data.connections.push({ type: computer?.type, id: computer.id })
 
+    setElements((els) => els.map(el => {
+      if (el.id === computer.id) {
+        el.data.connections = computer.data.connections
+      }
+      else if (el.id === component.id) {
+        el.data.connections = component.data.connections
+      }
+      return el
+    }));
+  }
+
+  const getElement = function (id: string | null): Node | null {
+    let element: any
+    if (id) {
+      setElements((es) => {
+        element = es.find(e => e.id === id)
+        return es
+      });
+    }
+
+    return element
+  }
+
+  const isValidConnection = (connection: Connection) => {
+    const { source, target } = connection
+    const computer = getElement(target)
+    const component = getElement(source)
+    if (!computer || !component) {
+      return false
+    }
+
+    // se o componente já tem conexão
+    const hasConnection = component.data.connections.length > 0
+    if (hasConnection) {
+      return false
+    }
+    // se o computador já tem um componente daquele tipo
+    const computerHastComponent = computer.data.connections.find((cp: any) =>
+      cp.type === component.type)
+    if (computerHastComponent) {
+      return false
+    }
+
+    console.log(true)
+    return true
+  }
+
+  //custom nodes
+
+  const CustomComputer: FC<NodeProps> = () => {
+    return (
+      <>
+        <img alt="computer" src={computador} width="100" height="100" />
+        <span>Computer </span>
+        <Handle type="target" position={Position.Right} isValidConnection={isValidConnection} />
+        <Handle type="target" position={Position.Left} isValidConnection={isValidConnection} />
+        <Handle type="target" position={Position.Bottom} isValidConnection={isValidConnection} />
+
+      </>
+    )
+  };
+
+  const CustomCpu: FC<NodeProps> = ({ type, data }) => {
+    return (<>
+      <Handle type="source" position={Position.Left} isValidConnection={isValidConnection} />
+      <img alt="cpu" src={cpu} width="100" height="100" />
+      <br></br>
+      CPU
+      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
+    </>)
+  };
+
+  const CustomDisk: FC<NodeProps> = ({ type, data }) => {
+    return (<>
+      <Handle type="source" position={Position.Left} isValidConnection={isValidConnection} />
+      <img alt="disk" src={disk} width="100" height="100" />
+      <br></br>
+      Disk
+      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
+    </>)
+  };
+
+  const CustomMemory: FC<NodeProps> = ({ type, data }) => {
+    return (<>
+      <Handle type="source" position={Position.Left} isValidConnection={isValidConnection} />
+      <img alt="memory" src={memory} width="100" height="100" />
+      <br></br>
+      Memory
+      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
+    </>)
+  };
+
+  const CustomNetwork: FC<NodeProps> = ({ type, data }) => {
+    return (<>
+      <Handle type="source" position={Position.Left} isValidConnection={isValidConnection} />
+      <img alt="network" src={network} width="100" height="100" />
+      <br></br>
+      Network
+      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
+    </>)
+  };
+
+  const CustomProcesses: FC<NodeProps> = ({ type, data }) => {
+    return (<>
+      <Handle type="source" position={Position.Left} isValidConnection={isValidConnection} />
+      <img alt="process" src={process} width="100" height="100" />
+      Processes
+      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
+    </>)
+  };
+
+  const nodeTypes: NodeTypesType = {
+    computer: CustomComputer,
+    cpu: CustomCpu,
+    disk: CustomDisk,
+    memory: CustomMemory,
+    network: CustomNetwork,
+    processes: CustomProcesses
+  };
 
 
   /// BUG: adicona dois nós de uma vez
@@ -47,7 +191,8 @@ const DnDFlow = () => {
         type,
         position,
         data: {
-          payload: 'a'
+          connections: [],
+          payload: {}
         }
       };
       setElements((es) => es.concat(newNode));
@@ -74,7 +219,6 @@ const DnDFlow = () => {
             onDrop={onDrop}
             className="validationflow"
             selectNodesOnDrag={false}
-
             onDragOver={onDragOver}
             onNodeDoubleClick={onElementClick}
           >
