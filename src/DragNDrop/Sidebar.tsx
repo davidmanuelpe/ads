8bcdf8d3d6477a89dc8377483ee61f9880d4bae9
matheus <matheus.noronha@solyd.com.br>
2021-12-01 11:15:18 -0300

    feat: custom node components

--- src/DragNDrop/Sidebar.tsx
+++ src/DragNDrop/Sidebar.tsx
@@ -11,54 +11,44 @@ const onDragStart = (event: DragEvent, nodeType: string) => {
   event.dataTransfer.effectAllowed = 'move';
 };
 
-// const onDragStart = (event: DragEvent, nodeType: string) => {
-//   event.dataTransfer.setDragImage(<img src={computador}, 0, 20);
-//   event.dataTransfer.effectAllowed = 'move';
-// };
-
-
-
-
 const Sidebar = () => {
   return (
     <aside>
 
       <div className="description">You can drag these nodes to the pane on the left.</div>
-      <div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'computador')} draggable>
-        <img src={computador} width="100" height="100" />
+      <div className="react-flow__node-default"
+        onDragStart={(event: DragEvent) => onDragStart(event, 'computer')} draggable>
+        <img alt="computer" src={computador} width="100" height="100" />
         Computer
       </div>
-      <div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>
-        <img src={cpu} width="100" height="100" />
+      <div className="react-flow__node-default"
+        onDragStart={(event: DragEvent) => onDragStart(event, 'cpu')} draggable>
+        <img alt="cpu" src={cpu} width="100" height="100" />
         <br></br>
         CPU
       </div>
-      <div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>
-        <img src={disk} width="100" height="100" />
+      <div className="react-flow__node-default"
+        onDragStart={(event: DragEvent) => onDragStart(event, 'disk')} draggable>
+        <img alt="disk" src={disk} width="100" height="100" />
         <br></br>
         Disk
       </div>
-      <div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>
-        <img src={memory} width="100" height="100" />
+      <div className="react-flow__node-default"
+        onDragStart={(event: DragEvent) => onDragStart(event, 'memory')} draggable>
+        <img alt="memory" src={memory} width="100" height="100" />
         <br></br>
         Memory
       </div>
-      <div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>
-        <img src={network} width="100" height="100" />
+      <div className="react-flow__node-default"
+        onDragStart={(event: DragEvent) => onDragStart(event, 'network')} draggable>
+        <img alt="network" src={network} width="100" height="100" />
         <br></br>
         Network
       </div>
-      <div className="react-flow__node-default" onDragStart={(event: DragEvent) => onDragStart(event, 'default')} draggable>
-        <img src={process} width="100" height="100" />
+      <div className="react-flow__node-default"
+        onDragStart={(event: DragEvent) => onDragStart(event, 'processes')} draggable>
+        <img alt="process" src={process} width="100" height="100" />
         Processes
-      </div>
-      <div className="react-flow__node-input" onDragStart={(event: DragEvent) => onDragStart(event, 'input')} draggable>
-        Blue Node
-      </div>
-
-
-      <div className="react-flow__node-output" onDragStart={(event: DragEvent) => onDragStart(event, 'output')} draggable>
-        Red Node
       </div>
     </aside>
   );
--- src/DragNDrop/index.tsx
+++ src/DragNDrop/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, DragEvent, FC } from 'react';
+import React, { useState, DragEvent, MouseEvent } from 'react';
 import ReactFlow, {
   ReactFlowProvider,
   addEdge,
@@ -10,82 +10,56 @@ import ReactFlow, {
   Edge,
   ElementId,
   Node,
-  Handle,
-  Position,
-  NodeProps,
-  NodeTypesType,
+  FlowElement,
 } from 'react-flow-renderer';
-import computador from '../imagens/computador.jpg';
+import { nodeTypes } from './CustomNodes'
 
 
 import Sidebar from './Sidebar';
 
 import './dnd.css';
 
-const initialElements = [
-  { id: '1', type: 'input', data: { label: 'input node' }, position: { x: 250, y: 5 } }
-];
-
-const onDragOver = (event: DragEvent) => {
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'move';
-};
 
 let id = 0;
-const getId = (): ElementId => `dndnode_${id++}`;
+export const getId = (): ElementId => `dndnode_${id++}`;
 
 const DnDFlow = () => {
   const [reactFlowInstance, setReactFlowInstance] = useState<OnLoadParams>();
-  const [elements, setElements] = useState<Elements>(initialElements);
-
-  const isValidConnection = (connection: Connection) => connection.target === 'B';
+  const [elements, setElements] = useState<Elements>([]);
   const onConnect = (params: Connection | Edge) => setElements((els) => addEdge(params, els));
   const onElementsRemove = (elementsToRemove: Elements) => setElements((els) => removeElements(elementsToRemove, els));
   const onLoad = (_reactFlowInstance: OnLoadParams) => setReactFlowInstance(_reactFlowInstance);
+  const onElementClick = (_: MouseEvent, element: FlowElement) => {
+    console.log(element)
+  }
 
+
+
+
+  /// BUG: adicona dois nós de uma vez
   const onDrop = (event: DragEvent) => {
     event.preventDefault();
-
-    if (reactFlowInstance) {
-      const type = event.dataTransfer.getData('application/reactflow');
+    const type = event.dataTransfer.getData('application/reactflow');
+    if (reactFlowInstance && type) {
       const position = reactFlowInstance.project({ x: event.clientX, y: event.clientY - 40 });
-      console.log(type)
       const newNode: Node = {
         id: getId(),
         type,
         position,
         data: {
-          label: type === 'computador' ?
-            (<div>
-              <img src={computador} width="100" height="100" />
-              <span>Computer </span>
-            </div>) : 'hello'
-        },
+          payload: 'a'
+        }
       };
-
       setElements((es) => es.concat(newNode));
     }
   };
 
-  const CustomInput: FC<NodeProps> = () => (
-    <>
-      <div>Only connectable with B</div>
-      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
-    </>
-  );
-
-  const CustomNode: FC<NodeProps> = ({ id }) => (
-    <>
-      <Handle type="target" position={Position.Left} isValidConnection={isValidConnection} />
-      <div>{id}</div>
-      <Handle type="source" position={Position.Right} isValidConnection={isValidConnection} />
-    </>
-  );
-
-  const nodeTypes: NodeTypesType = {
-    computador: CustomInput,
-    customnode: CustomNode,
+  const onDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
   };
+
+
 
   return (
     <div className="dndflow">
@@ -98,7 +72,11 @@ const DnDFlow = () => {
             onElementsRemove={onElementsRemove}
             onLoad={onLoad}
             onDrop={onDrop}
+            className="validationflow"
+            selectNodesOnDrag={false}
+
             onDragOver={onDragOver}
+            onNodeDoubleClick={onElementClick}
           >
             <Controls />
           </ReactFlow>
